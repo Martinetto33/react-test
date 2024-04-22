b3338db10937d999817b21eb1320f6a869f71846
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-04-22 15:29:06 +0200

    feat: started to add memento

--- src/App.js
+++ src/App.js
@@ -57,18 +57,38 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
   const currentSquares = history[history.length - 1]
+
   function handlePlay(nextSquares) {
     setHistory([...history, nextSquares]) /* ...history means 'enumerate all elements in history' 
     [this is called spread syntax], then append nextSquares */
     setXIsNext(!xIsNext)
   }
+
+  function jumpTo(nextMove) {
+
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move
+    } else {
+      description = 'Go to game start'
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/* TODO */}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
